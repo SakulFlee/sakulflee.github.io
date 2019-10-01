fdf917e4e58b2740a37e65fed13fab3199c2d8cd
@Sakul6499 | Lukas Weber <me@sakul6499.de>
2019-10-01 15:46:40 +0200

    Added comment
    Signed-off-by: @Sakul6499 | Lukas Weber <me@sakul6499.de>

--- src/index.tsx
+++ src/index.tsx
@@ -4,6 +4,7 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
+// Bulma
 import 'bulma/css/bulma.css';
 
 ReactDOM.render(<App />, document.getElementById('root'));
