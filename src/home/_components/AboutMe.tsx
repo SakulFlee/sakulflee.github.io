a8b46d0906424d1eaef745d907205401d3ace581
@Sakul6499 | Lukas Weber <me@sakul6499.de>
2019-10-03 19:33:54 +0200

    Updated AboutMe
    Signed-off-by: @Sakul6499 | Lukas Weber <me@sakul6499.de>

--- src/home/_components/AboutMe.tsx
+++ src/home/_components/AboutMe.tsx
@@ -1,14 +1,27 @@
 import React from "react";
 import { Link } from "react-router-dom";
 
+import Tag from '../../_components/Tag';
+
+import './AboutMe.scss';
+
 class AboutMe extends React.Component {
   render() {
     return (
-      <section>
-        <p>AboutMe</p>
+      <section className="columns has-text-centered" id="AboutMe">
+        <article className="column">
+          <u className="is-half is-family-code has-text-underlined">
+            About me
+          </u>
+        </article>
+        <aside className="column">
+          <u className="is-half is-family-code has-text-underlined tooltip is-tooltip-multiline is-tooltip-active is-tooltip-info" data-tooltip="Hover over my tags!">
+            Skills
+          </u>
+          <h1 className="is-size-5 has-text-black">Languages I speak:</h1>
 
-        <Link to="/">Home</Link>
-        <Link to="/blog">Blog</Link>
+          <Tag text="Test" tooltip="Hello, world!" linkTo="/"/>
+        </aside>
       </section>
     );
   }
