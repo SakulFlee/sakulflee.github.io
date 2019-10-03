6c0865e0f996187bba0164847ad3b3d3ab98dea4
@Sakul6499 | Lukas Weber <me@sakul6499.de>
2019-10-03 19:33:42 +0200

    Added Tag
    Signed-off-by: @Sakul6499 | Lukas Weber <me@sakul6499.de>

---
+++ src/_components/Tag.tsx
@@ -0,0 +1,37 @@
+import React from 'react';
+import {Link} from 'react-router-dom';
+
+type TagProperties = {
+    text: string,
+    tooltip: string,
+    tooltipOnly?: boolean,
+    active?: boolean,
+    multiLine?: boolean,
+    underline?: boolean,
+    colorClass?: string,
+    tooltipClass?: string,
+    textColor?: string,
+    linkTo?: string,
+}
+
+type TagState = {}
+
+export default class Tag extends React.Component<TagProperties, TagState> {
+    static defaultProps = {
+        tooltipOnly: false,
+        active: false,
+        multiLine: false,
+        underline: false,
+        colorClass: "is-info",
+        tooltipClass: "is-tooltip-info",
+        textColor: "has-text-white",
+    }
+
+    render(): JSX.Element {
+        return (
+            <div className={`is-half is-family-code ${this.props.textColor} ${this.props.tooltipOnly ? "" : "tag"} ${this.props.underline ? "has-text-underlined" : ""} tooltip ${this.props.multiLine ? "is-tooltip-multiline" : ""} ${this.props.active ? "is-tooltip-active" : ""} ${this.props.colorClass} ${this.props.tooltipClass}`} data-tooltip={this.props.tooltip}>
+                {this.props.linkTo == null ? this.props.text : <Link className={`${this.props.textColor}`} to={this.props.linkTo}>{this.props.text}</Link>} 
+            </div>
+        );
+    }
+}
