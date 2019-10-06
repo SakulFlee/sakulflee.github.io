d8bfcf3fecb19e1e15bce9eaea072a4bae31f17a
@Sakul6499 | Lukas Weber <me@sakul6499.de>
2019-10-06 12:31:32 +0200

    Replaced line with gradient and text effects
    Signed-off-by: @Sakul6499 | Lukas Weber <me@sakul6499.de>

--- src/home/_components/TitlePage.tsx
+++ src/home/_components/TitlePage.tsx
@@ -3,7 +3,6 @@ import {Link} from 'react-router-dom';
 import {FaExternalLinkAlt} from 'react-icons/fa';
 
 import Cube from '../../_components/Cube';
-import Line from '../../_components/Line';
 
 import './TitlePage.scss';
 
@@ -16,9 +15,8 @@ class TitlePage extends React.Component<{}> {
         <Cube size={48} left={80} top={20} colour={"magenta"} animation={"pulse 1s infinite"} />
         <Cube size={64} left={82} top={44} colour={"yellowgreen"} animation={"color 15s infinite"} />
         <Cube size={72} left={62} top={30} colour={"orange"} animation={"glow 3s infinite"} />
-        <Line degree={150} />
 
-        <section className="hero is-dark is-fullheight">
+        <section className="hero is-dark is-fullheight" id="title-page">
           <div className="hero-body">
             <div className="container" id="title-text">
               <h1 className="title has-text-danger is-family-code">
@@ -26,6 +24,12 @@ class TitlePage extends React.Component<{}> {
               </h1>
               <h2 className="subtitle">I am Lukas Weber | @Sakul6499.</h2>
               <p className="is-family-code">Software Engineer Student</p>
+
+              <div id="binary">
+              <p className="has-text-right has-text-grey">01001000 01100101 01101100 01101100 01101111 00101100</p>
+              <p className="has-text-right has-text-grey">00100000 01110111 01101111 01110010 01101100 01100100</p>
+              <p className="has-text-right has-text-grey">00100001</p>
+              </div>
             </div>
           </div>
         </section>
