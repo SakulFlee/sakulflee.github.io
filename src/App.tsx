0d7ac97b8ae75f412e14be3415ce28e761b51e37
@Sakul6499 | Lukas Weber <me@sakul6499.de>
2019-10-06 23:11:18 +0200

    Added old sides to routing
    Signed-off-by: @Sakul6499 | Lukas Weber <me@sakul6499.de>

--- src/App.tsx
+++ src/App.tsx
@@ -3,11 +3,15 @@ import {BrowserRouter as Router, Route} from 'react-router-dom';
 
 import Home from './home/Home';
 import BlogPosts from './blog/BlogPosts';
+import Credit from './home/Credit';
+import Legal from './home/Legal';
 
 const App: React.FC = () => {
   return (
     <Router>
       <Route exact={true}   path="/"          component={Home}        />
+      <Route exact={true}   path="/credit"    component={Credit}      />
+      <Route exact={true}   path="/disclaimer_legal_and_privacy"  component={Legal} />
       <Route exact={true}   path="/blog"      component={BlogPosts}   />
       <Route exact={false}  path="/blog/:id"  component={BlogPosts}   />
     </Router>
