6b6329abd130baa27fc734d1d79588c76b210f72
@Sakul6499 | Lukas Weber <me@sakul6499.de>
2019-10-09 07:39:10 +0200

    Added Skills page
    Signed-off-by: @Sakul6499 | Lukas Weber <me@sakul6499.de>

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import {BrowserRouter as Router, Route} from 'react-router-dom';
 
 import Home from './home/Home';
+import Skills from './home/Skills';
 import BlogPosts from './blog/BlogPosts';
 import Credit from './home/Credit';
 import Legal from './home/Legal';
@@ -10,6 +11,7 @@ const App: React.FC = () => {
   return (
     <Router>
       <Route exact={true}   path="/"          component={Home}        />
+      <Route exact={true}   path="/skills"    component={Skills} />
       <Route exact={true}   path="/credit"    component={Credit}      />
       <Route exact={true}   path="/disclaimer_legal_and_privacy"  component={Legal} />
       <Route exact={true}   path="/blog"      component={BlogPosts}   />
