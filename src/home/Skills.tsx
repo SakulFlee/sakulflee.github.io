9c4a58489e612695e518384430b3999d1651cb3e
@Sakul6499 | Lukas Weber <me@sakul6499.de>
2019-10-09 07:50:50 +0200

    Basis for skills page
    Signed-off-by: @Sakul6499 | Lukas Weber <me@sakul6499.de>

--- src/home/Skills.tsx
+++ src/home/Skills.tsx
@@ -1,7 +1,269 @@
-import React from 'react';
+import React from "react";
 
 export default class Skills extends React.Component {
-    render(): JSX.Element {
-        return (<div></div>);
-    }
+  render(): JSX.Element {
+    return (
+      <div>
+        <section className="hero is-medium is-light is-bold">
+          <div className="hero-body">
+            <div className="container">
+              <h1 className="title">Skills</h1>
+              <h2 className="subtitle">...</h2>
+            </div>
+          </div>
+        </section>
+
+        <article className="columns">
+        <div className="tile is-parent no-padding-bottom column">
+          <div className="tile is-parent">
+            <article className="tile is-child notification is-dark">
+              <p className="title" id="Languages">
+                Languages
+              </p>
+              <p>asd</p>
+            </article>
+          </div>
+        </div>
+
+        <div className="tile is-parent no-padding-bottom column">
+          <div className="tile is-parent">
+            <article className="tile is-child notification is-dark">
+              <p className="title" id="Native">
+                Native
+              </p>
+              <p>asd</p>
+            </article>
+          </div>
+        </div>
+        </article>
+
+        <article className="columns">
+        <div className="tile is-parent no-padding-bottom column">
+          <div className="tile is-parent">
+            <article className="tile is-child notification is-dark">
+              <p className="title" id="Web">
+                Web
+              </p>
+              <p>asd</p>
+            </article>
+          </div>
+        </div>
+
+        <div className="tile is-parent no-padding-bottom column">
+          <div className="tile is-parent">
+            <article className="tile is-child notification is-dark">
+              <p className="title" id="Rust">
+                Rust
+              </p>
+              <p>asd</p>
+            </article>
+          </div>
+        </div>
+        </article>
+
+        <article className="columns">
+        <div className="tile is-parent no-padding-bottom column">
+          <div className="tile is-parent">
+            <article className="tile is-child notification is-dark">
+              <p className="title" id="Office">
+                Office
+              </p>
+              <p>asd</p>
+            </article>
+          </div>
+        </div>
+
+        <div className="tile is-parent no-padding-bottom column">
+          <div className="tile is-parent">
+            <article className="tile is-child notification is-dark">
+              <p className="title" id="/skillsLanguages">
+                /skillsLanguages
+              </p>
+              <p>asd</p>
+            </article>
+          </div>
+        </div>
+        </article>
+
+        <article className="columns">
+        <div className="tile is-parent no-padding-bottom column">
+          <div className="tile is-parent">
+            <article className="tile is-child notification is-dark">
+              <p className="title" id="/skills#JVMsimilar">
+                /skills#JVMsimilar
+              </p>
+              <p>asd</p>
+            </article>
+          </div>
+        </div>
+
+        <div className="tile is-parent no-padding-bottom column">
+          <div className="tile is-parent">
+            <article className="tile is-child notification is-dark">
+              <p className="title" id="VCS">
+                VCS
+              </p>
+              <p>asd</p>
+            </article>
+          </div>
+        </div>
+        </article>
+
+        <article className="columns">
+        <div className="tile is-parent no-padding-bottom column">
+          <div className="tile is-parent">
+            <article className="tile is-child notification is-dark">
+              <p className="title" id="/skills#IDEsEditor">
+                /skills#IDEsEditor
+              </p>
+              <p>asd</p>
+            </article>
+          </div>
+        </div>
+
+        <div className="tile is-parent no-padding-bottom column">
+          <div className="tile is-parent">
+            <article className="tile is-child notification is-dark">
+              <p className="title" id="/skillsTools">
+                /skillsTools
+              </p>
+              <p>asd</p>
+            </article>
+          </div>
+        </div>
+        </article>
+
+        <article className="columns">
+        <div className="tile is-parent no-padding-bottom column">
+          <div className="tile is-parent">
+            <article className="tile is-child notification is-dark">
+              <p className="title" id="Modelling">
+                Modelling
+              </p>
+              <p>asd</p>
+            </article>
+          </div>
+        </div>
+
+        <div className="tile is-parent no-padding-bottom column">
+          <div className="tile is-parent">
+            <article className="tile is-child notification is-dark">
+              <p className="title" id="Office">
+                Office
+              </p>
+              <p>asd</p>
+            </article>
+          </div>
+        </div>
+        </article>
+
+        <article className="columns">
+        <div className="tile is-parent no-padding-bottom column">
+          <div className="tile is-parent">
+            <article className="tile is-child notification is-dark">
+              <p className="title" id="Virtualization">
+                Virtualization
+              </p>
+              <p>asd</p>
+            </article>
+          </div>
+        </div>
+
+        <div className="tile is-parent no-padding-bottom column">
+          <div className="tile is-parent">
+            <article className="tile is-child notification is-dark">
+              <p className="title" id="/skills#TestDevelopment">
+                /skills#TestDevelopment
+              </p>
+              <p>asd</p>
+            </article>
+          </div>
+        </div>
+        </article>
+
+        <article className="columns">
+        <div className="tile is-parent no-padding-bottom column">
+          <div className="tile is-parent">
+            <article className="tile is-child notification is-dark">
+              <p className="title" id="Linux">
+                Linux
+              </p>
+              <p>asd</p>
+            </article>
+          </div>
+        </div>
+
+        <div className="tile is-parent no-padding-bottom column">
+          <div className="tile is-parent">
+            <article className="tile is-child notification is-dark">
+              <p className="title" id="/skillsSystems">
+                /skillsSystems
+              </p>
+              <p>asd</p>
+            </article>
+          </div>
+        </div>
+        </article>
+
+        <article className="columns">
+        <div className="tile is-parent no-padding-bottom column">
+          <div className="tile is-parent">
+            <article className="tile is-child notification is-dark">
+              <p className="title" id="Hacking">
+                Hacking
+              </p>
+              <p>asd</p>
+            </article>
+          </div>
+        </div>
+
+        <div className="tile is-parent no-padding-bottom column">
+          <div className="tile is-parent">
+            <article className="tile is-child notification is-dark">
+              <p className="title" id="AI">
+                AI
+              </p>
+              <p>asd</p>
+            </article>
+          </div>
+        </div>
+        </article>
+
+        <article className="columns">
+        <div className="tile is-parent no-padding-bottom column">
+          <div className="tile is-parent">
+            <article className="tile is-child notification is-dark">
+              <p className="title" id="IoT">
+                IoT
+              </p>
+              <p>asd</p>
+            </article>
+          </div>
+        </div>
+
+        <div className="tile is-parent no-padding-bottom column">
+          <div className="tile is-parent">
+            <article className="tile is-child notification is-dark">
+              <p className="title" id="Games">
+                Games
+              </p>
+              <p>asd</p>
+            </article>
+          </div>
+        </div>
+        </article>
+
+        <div className="tile is-parent no-padding-bottom column">
+          <div className="tile is-parent">
+            <article className="tile is-child notification is-dark">
+              <p className="title" id="Graphics">
+                Graphics
+              </p>
+              <p>asd</p>
+            </article>
+          </div>
+        </div>
+      </div>
+    );
+  }
 }
