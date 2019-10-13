d769db87fb0d3dda478a61ff3f148cb098b434c4
@Sakul6499 | Lukas Weber <me@sakul6499.de>
2019-10-13 17:49:37 +0200

    Cleanup of Tag
    Signed-off-by: @Sakul6499 | Lukas Weber <me@sakul6499.de>

--- src/_components/Tag.tsx
+++ src/_components/Tag.tsx
@@ -1,39 +1,53 @@
-import React from 'react';
-import {Link} from 'react-router-dom';
+import React from "react";
+import { Link } from "react-router-dom";
 
-import './Tag.scss';
+import "./Tag.scss";
 
 type TagProperties = {
-    text: string,
-    tooltip: string,
-    color: string,
-    tooltipOnly?: boolean,
-    active?: boolean,
-    multiLine?: boolean,
-    underline?: boolean,
-    size?: string,
-    linkTo?: string,
-}
+  text: string;
+  tooltip: string;
+  color: string;
+  tooltipOnly?: boolean;
+  active?: boolean;
+  multiLine?: boolean;
+  underline?: boolean;
+  size?: string;
+  linkTo?: string;
+};
 
-type TagState = {}
+type TagState = {};
 
 export default class Tag extends React.Component<TagProperties, TagState> {
-    static defaultProps = {
-        tooltipOnly: false,
-        active: false,
-        multiLine: false,
-        underline: false,
-        tooltipClass: "is-tooltip-info",
-        textColor: "has-text-white",
-        size: "",
-    }
+  static defaultProps = {
+    tooltipOnly: false,
+    active: false,
+    multiLine: false,
+    underline: false,
+    tooltipClass: "is-tooltip-info",
+    textColor: "has-text-white",
+    size: ""
+  };
 
-    render(): JSX.Element {
-        let textColor = (this.props.color === 'warning' || this.props.color === 'white') ? 'has-text-black' : 'has-text-white';
-        return (
-            <div className={`is-half is-family-code ${textColor} ${this.props.tooltipOnly ? "" : "tag"} ${this.props.underline ? "has-text-underlined" : ""} tooltip ${this.props.multiLine ? "is-tooltip-multiline" : ""} ${this.props.active ? "is-tooltip-active" : ""} is-${this.props.color} is-tooltip-${this.props.color} ${this.props.size}`} data-tooltip={this.props.tooltip}>
-                {this.props.linkTo == null ? this.props.text : <Link className={`${textColor}`} to={this.props.linkTo}>{this.props.text}</Link>} 
-            </div>
-        );
+  render(): JSX.Element {
+    let textColor =
+      this.props.color === "warning" || this.props.color === "white"
+        ? "has-text-black"
+        : "has-text-white";
+
+    let classes = `is-half is-family-code tooltip ${textColor} is-${this.props.color} is-tooltip-${this.props.color} ${this.props.size}`;
+    if (this.props.tooltipOnly) classes += "tag";
+    if (this.props.underline) classes += "has-text-underlined";
+    if (this.props.multiLine) classes += "is-tooltip-multiline";
+    if (this.props.active) classes += "is-tooltip-active";
+
+    if (this.props.linkTo == null) {
+      return <div>{this.props.text}</div>;
+    } else {
+      return (
+        <Link to={this.props.linkTo} className={textColor}>
+          {this.props.text}
+        </Link>
+      );
     }
+  }
 }
