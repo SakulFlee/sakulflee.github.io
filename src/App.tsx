306174a4b11f22ab2213fc04b773609b1f178984
@Sakul6499 | Lukas Weber <me@sakul6499.de>
2019-10-13 17:48:51 +0200

    Cleaned App.tsx
    Signed-off-by: @Sakul6499 | Lukas Weber <me@sakul6499.de>

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,25 @@
-import React from 'react';
-import {BrowserRouter as Router, Route} from 'react-router-dom';
+import React from "react";
+import { BrowserRouter as Router, Route } from "react-router-dom";
 
-import Home from './home/Home';
-import Skills from './home/Skills';
-import BlogPosts from './blog/BlogPosts';
-import Credit from './home/Credit';
-import Legal from './home/Legal';
+import Home from "./home/Home";
+import BlogHome from "./blog/BlogHome";
+import BlogPosts from "./blog/BlogPost";
+import Credit from "./home/Credit";
+import Legal from "./home/Legal";
 
 const App: React.FC = () => {
   return (
     <Router>
-      <Route exact={true}   path="/"          component={Home}        />
-      <Route exact={true}   path="/skills"    component={Skills} />
-      <Route exact={true}   path="/credit"    component={Credit}      />
-      <Route exact={true}   path="/disclaimer_legal_and_privacy"  component={Legal} />
-      <Route exact={true}   path="/blog"      component={BlogPosts}   />
-      <Route exact={false}  path="/blog/:id"  component={BlogPosts}   />
+      <Route exact={true} path="/" component={Home} />
+      <Route exact={true} path="/credit" component={Credit} />
+      <Route
+        exact={true}
+        path="/disclaimer_legal_and_privacy"
+        component={Legal}
+      />
+      <Route exact={true} path="/blog" component={BlogHome} />
+      <Route exact={false} path="/blog/:id" component={BlogPosts} />
     </Router>
   );
-}
+};
 export default App;
