9579b02a0d9996b060b4264d223b546e61b73526
@Sakul6499 | Lukas Weber <me@sakul6499.de>
2019-10-14 16:08:38 +0200

    Prevent multiple pages to be rendered at once
    Signed-off-by: @Sakul6499 | Lukas Weber <me@sakul6499.de>

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter as Router, Route } from "react-router-dom";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 
 import Home from "./home/Home";
 import BlogHome from "./blog/BlogHome";
@@ -10,15 +10,17 @@ import Legal from "./home/Legal";
 const App: React.FC = () => {
   return (
     <Router>
-      <Route exact={true} path="/credit" component={Credit} />
-      <Route
-        exact={true}
-        path="/disclaimer_legal_and_privacy"
-        component={Legal}
-      />
-      <Route exact={true} path="/blog" component={BlogHome} />
-      <Route exact={false} path="/blog/:id" component={BlogPosts} />
-      <Route exact={false} path="/" component={Home} />
+      <Switch>
+        <Route exact={true} path="/" component={Home} />
+        <Route exact={true} path="/credit" component={Credit} />
+        <Route
+          exact={true}
+          path="/disclaimer_legal_and_privacy"
+          component={Legal}
+        />
+        <Route exact={true} path="/blog" component={BlogHome} />
+        <Route exact={false} path="/blog/:id" component={BlogPosts} />
+      </Switch>
     </Router>
   );
 };
