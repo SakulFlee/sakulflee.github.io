3f9280440ca346c2d577b3ca3b7c9a1a794193c7
@Sakul6499 | Lukas Weber <me@sakul6499.de>
2019-10-16 11:09:36 +0200

    Possibly fixed reloading issue when scrolling
    Signed-off-by: @Sakul6499 | Lukas Weber <me@sakul6499.de>

--- src/home/_components/Header.tsx
+++ src/home/_components/Header.tsx
@@ -1,60 +1,67 @@
 import React from "react";
 
-const Trianglify = require('trianglify');
+const Trianglify = require("trianglify");
 
 type HeaderProperties = {
-    maxHeightInPercent: number,
+  maxHeightInPercent: number;
 };
 
 type HeaderState = {
-    width: number,
-    height: number,
+  width: number;
+  height: number;
+  pattern: any;
 };
 
-export default class Header extends React.Component<HeaderProperties, HeaderState> {
-    static defaultProps: HeaderProperties = {
-        maxHeightInPercent: 100,
+export default class Header extends React.Component<
+  HeaderProperties,
+  HeaderState
+> {
+  static defaultProps: HeaderProperties = {
+    maxHeightInPercent: 100
+  };
+
+  updateDimensions = () => {
+    let maxHeight = window.innerHeight * (this.props.maxHeightInPercent / 100);
+    let maxWidth = window.innerWidth;
+
+    let pattern = Trianglify({
+      width: maxWidth,
+      height: maxHeight,
+      cell_size: 30 + Math.random() * 100
+    });
+
+    this.setState({
+      width: maxWidth,
+      height: maxHeight,
+      pattern: pattern
+    });
+  };
+
+  componentDidMount() {
+    this.updateDimensions();
+    window.addEventListener("resize", this.updateDimensions);
+  }
+
+  componentWillUnmount() {
+    window.removeEventListener("resize", this.updateDimensions);
+  }
+
+  render(): JSX.Element {
+    if (this.state == null) return <p>Loading ...</p>;
+
+    let style = {
+      width: `${this.state.width}px`,
+      height: `${this.state.height}px`,
+      position: "static" as "static",
+      top: 0,
+      left: 0
     };
 
-    updateDimensions = () => {
-        let maxHeight = window.innerHeight * (this.props.maxHeightInPercent / 100);
-        this.setState({
-            width: window.innerWidth,
-            height: maxHeight
-        });
-    };
-
-    componentDidMount() {
-        this.updateDimensions();
-        window.addEventListener('resize', this.updateDimensions);
-    }
-
-    componentWillUnmount() {
-        window.removeEventListener('resize', this.updateDimensions);
-    }
-
-    render(): JSX.Element {
-        if (this.state == null) return <p>Loading ...</p>;
-
-        let pattern = Trianglify({
-            width: this.state.width, 
-            height: this.state.height,
-            cell_size: 30 + Math.random() * 100
-        });
-
-        let style = {
-            width: `${this.state.width}px`,
-            height: `${this.state.height}px`,
-            position: 'static' as 'static',
-            top: 0,
-            left: 0,
-        };
-
-        return (
-                <svg 
-                    style={style} 
-                    dangerouslySetInnerHTML={{__html: pattern.svg().innerHTML}}
-                />
-        );
-    }
+    return (
+      <svg
+        style={style}
+        dangerouslySetInnerHTML={{ __html: this.state.pattern.svg().innerHTML }}
+      />
+    );
+  }
 }
