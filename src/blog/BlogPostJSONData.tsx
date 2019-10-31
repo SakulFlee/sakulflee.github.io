2ed262c1e278e848d73a48171adb655019115f8b
@Sakul6499 | Lukas Weber <me@sakul6499.de>
2019-10-31 15:18:10 +0100

    Added a short-description field to BlogPostJSONData

--- src/blog/BlogPostJSONData.tsx
+++ src/blog/BlogPostJSONData.tsx
@@ -2,6 +2,7 @@ const markdown = require("markdown").markdown;
 
 export default class BlogPostJSONData {
   public id: number;
+  public shortDescription: string;
   public description: string;
   public tags: string[];
   public path: string;
@@ -10,11 +11,39 @@ export default class BlogPostJSONData {
   private predefinedTitle: string | null = null;
   private preFetchedMarkdown: string | null = null;
 
+  constructor(
+      id: number,
+      shortDescription: string,
+      description: string,
+      tags: string[],
+      path: string,
+      finished: boolean
+  ) {
+    this.id = id;
+    this.shortDescription = shortDescription;
+    this.description = description;
+    this.tags = tags;
+    this.path = path;
+    this.finished = finished;
+  }
+
+  public static async FetchByID(id: number): Promise<BlogPostJSONData> {
+    const response: Response = await fetch("/api/blog/posts.json");
+    const json: BlogPostJSONData[] = await response.json();
+    let post: BlogPostJSONData | undefined = json.find(it => it.id === id);
+    if (post == null) {
+      return BlogPostJSONData.CreateInvalidPost();
+    } else {
+      return BlogPostJSONData.FullFetch(post);
+    }
+  }
+
   public static async FullFetch(
     raw: BlogPostJSONData
   ): Promise<BlogPostJSONData> {
     let newPost = new BlogPostJSONData(
       raw.id,
+        raw.shortDescription,
       raw.description,
       raw.tags,
       raw.path,
@@ -33,39 +62,16 @@ export default class BlogPostJSONData {
     return newPost;
   }
 
-  public static async FetchByID(id: number): Promise<BlogPostJSONData> {
-    const response: Response = await fetch("/api/blog/posts.json");
-    const json: BlogPostJSONData[] = await response.json();
-    let post: BlogPostJSONData | undefined = json.find(it => it.id === id);
-    if (post == null) {
-      return BlogPostJSONData.CreateInvalidPost();
-    } else {
-      return BlogPostJSONData.FullFetch(post);
-    }
-  }
-
   public static CreateInvalidPost(): BlogPostJSONData {
     let post = new BlogPostJSONData(
       0,
+        "Either this post doesn't exist or an error happened in our backend.",
       "Either this post doesn't exist or an error happened in our backend.",
       [],
       "",
       true
     );
     post.predefinedTitle = "# Invalid or not found!";
-
-    return post;
-  }
-
-  public static CreateNothingFoundPost(): BlogPostJSONData {
-    let post = new BlogPostJSONData(
-      0,
-      "There are currently no posts available or the search didn't yield any result.",
-      [],
-      "",
-      false
-    );
-    post.predefinedTitle = "# No results!";
 
     return post;
   }
@@ -91,17 +97,17 @@ export default class BlogPostJSONData {
     }
   }
 
-  constructor(
-    id: number,
-    description: string,
-    tags: string[],
-    path: string,
-    finished: boolean
-  ) {
-    this.id = id;
-    this.description = description;
-    this.tags = tags;
-    this.path = path;
-    this.finished = finished;
+  public static CreateNothingFoundPost(): BlogPostJSONData {
+    let post = new BlogPostJSONData(
+        0,
+        "There are currently no posts available or the search didn't yield any result.",
+        "There are currently no posts available or the search didn't yield any result.",
+        [],
+        "",
+        false
+    );
+    post.predefinedTitle = "# No results!";
+
+    return post;
   }
 }
