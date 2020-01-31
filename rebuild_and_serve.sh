ed651c88db500dc18fb0973deb48ce109b78cd54
Lukas Weber <me@sakul6499.de>
2020-01-31 17:25:00 +0100

    Updated rebuild_and_serve.sh
    Signed-off-by: Lukas Weber <me@sakul6499.de>

--- rebuild_and_serve.sh
+++ rebuild_and_serve.sh
@@ -1,4 +1,5 @@
 VERSION="sakul6499-$(git rev-parse --abbrev-ref HEAD)"
+BUILD_VERSION="$VERSION-build"
 echo "Version: $VERSION"
 
 if [ "$VERSION" == "sakul6499-staging" ]; then
@@ -7,8 +8,8 @@ else
     PORT="8080:80"
 fi
 
-yarn install
-yarn build
+docker build -f build.docker -t $BUILD_VERSION .
+docker run -it --rm -v ${PWD}:/project $BUILD_VERSION bash -c "cd /project && yarn install && yarn build" 
 
 docker build -f serve.docker -t $VERSION .
 docker rm -f -v $VERSION
