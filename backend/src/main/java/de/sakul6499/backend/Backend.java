b43403288f061a7d11dace30c3baea4882b8da6b
Lukas Weber <me@sakul6499.de>
2020-02-09 02:34:44 +0100

    Updated Backend (main) class to set port to 8081 (instead of 8080)

--- backend/src/main/java/de/sakul6499/backend/Backend.java
+++ backend/src/main/java/de/sakul6499/backend/Backend.java
@@ -1,17 +1,19 @@
 package de.sakul6499.backend;
 
-import com.mongodb.MongoClient;
 import de.sakul6499.backend.data.MongoSettings;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.Collections;
+
 @SpringBootApplication
 public class Backend {
 
-	public static MongoSettings MONGO_SETTINGS = MongoSettings.AutoConfig();
+    public static MongoSettings MONGO_SETTINGS = MongoSettings.AutoConfig();
 
-	public static void main(String[] args) {
-		System.out.println("Settings:\r\n" + MONGO_SETTINGS.toString());
-		SpringApplication.run(Backend.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication app = new SpringApplication(Backend.class);
+        app.setDefaultProperties(Collections.singletonMap("server.port", "8081"));
+        app.run(args);
+    }
 }
