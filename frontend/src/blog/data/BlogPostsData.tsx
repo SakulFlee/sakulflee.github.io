41c2768622aa55cdd44133b5fc930da342b88404
Lukas Weber <me@sakul6499.de>
2020-02-09 21:58:51 +0100

    Utilizing Env file in BlogPostsData

--- frontend/src/blog/data/BlogPostsData.tsx
+++ frontend/src/blog/data/BlogPostsData.tsx
@@ -1,8 +1,32 @@
+import getURL from "../../Env";
 import BlogPostData from "./BlogPostData";
 
 export default class BlogPostsData {
 
-    private readonly blogPosts: BlogPostData[];
+    private readonly blogPostData: BlogPostData[];
+
+    public static async QueryRecent(limit: number = 10): Promise<BlogPostsData> {
+        let response = await fetch(`${getURL()}/api/blog/posts/recent?limit=${limit}`);
+        let json = await response.json();
+        let shadowedBlogPostsData = json as BlogPostsData;
+        return this.FromShadowed(shadowedBlogPostsData);
+    }
+
+    public static async QuerySearch(query: string): Promise<BlogPostsData> {
+        let response = await fetch(`${getURL()}/api/blog/posts/search?query=${query}`);
+        let json = await response.json();
+        let shadowedBlogPostsData = json as BlogPostsData;
+        return this.FromShadowed(shadowedBlogPostsData);
+    }
+
+    public static FromShadowed(shadowed: BlogPostsData): BlogPostsData {
+        let newBlogPosts: BlogPostData[] = [];
+        shadowed.blogPostData.forEach((shadowedBlogPost) => {
+            let newBlogPost = BlogPostData.FromShadowed(shadowedBlogPost);
+            newBlogPosts.push(newBlogPost);
+        });
+        return new BlogPostsData(newBlogPosts);
+    }
 
     constructor(blogPosts: BlogPostData[]) {
         this.blogPosts = blogPosts;
