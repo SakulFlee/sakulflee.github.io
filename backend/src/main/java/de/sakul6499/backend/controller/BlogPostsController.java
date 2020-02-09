04ab88b9b2d8756523447a53820d2b867ddefebd
Lukas Weber <me@sakul6499.de>
2020-02-09 02:33:40 +0100

    Updated BlogPostsController

--- backend/src/main/java/de/sakul6499/backend/controller/BlogPostsController.java
+++ backend/src/main/java/de/sakul6499/backend/controller/BlogPostsController.java
@@ -1,13 +1,19 @@
 package de.sakul6499.backend.controller;
 
-import de.sakul6499.backend.data.BlogPosts;
+import de.sakul6499.backend.data.BlogPostsData;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class BlogPostsController {
-    @GetMapping("/api/blog/posts")
-    public BlogPosts callback() {
-        return BlogPosts.Query();
+    @GetMapping("/api/blog/posts/recent")
+    public BlogPostsData callback(@RequestParam(name = "limit", defaultValue = "10") int limit) {
+        return BlogPostsData.QueryRecent(limit);
+    }
+
+    @GetMapping("/api/blog/posts/search")
+    public BlogPostsData callback(@RequestParam(name = "query") String query) {
+        return BlogPostsData.Search(query);
     }
 }
