d8bd3a4b8da3f5a959d36b2e2322c7294765400b
Lukas Weber <me@sakul6499.de>
2020-02-09 02:33:31 +0100

    Updated BlogPostController

--- backend/src/main/java/de/sakul6499/backend/controller/BlogPostController.java
+++ backend/src/main/java/de/sakul6499/backend/controller/BlogPostController.java
@@ -1,14 +1,14 @@
 package de.sakul6499.backend.controller;
 
-import de.sakul6499.backend.data.BlogPost;import org.springframework.web.bind.annotation.GetMapping;import org.springframework.web.bind.annotation.RequestParam;import org.springframework.web.bind.annotation.RestController;
-
-import java.util.LinkedList;
-import java.util.concurrent.atomic.AtomicLong;
+import de.sakul6499.backend.data.BlogPostData;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class BlogPostController {
-    @GetMapping("/api/blog/post")
-    public BlogPost callback(@RequestParam(value = "id") long id) {
-        return BlogPost.FromID(id);
+    @GetMapping("/api/blog/post/{id}")
+    public BlogPostData callback(@PathVariable(value = "id") long id) {
+        return BlogPostData.FromID(id);
     }
 }
