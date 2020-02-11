6267d1ab76760938f12feddce793bea5f806ce9a
Lukas Weber <me@sakul6499.de>
2020-02-11 23:15:26 +0100

    Reformatted BlogPosts
    Signed-off-by: Lukas Weber <me@sakul6499.de>

--- frontend/src/blog/BlogPosts.tsx
+++ frontend/src/blog/BlogPosts.tsx
@@ -1,84 +1,102 @@
 import React from "react";
-import {Link} from "react-router-dom";
+import { Link } from "react-router-dom";
 
 import BlogPostData from "./data/BlogPostData";
 import "./BlogPosts.scss";
 import BlogPostsData from "./data/BlogPostsData";
 
 type BlogPostsProperties = {
-    search?: string;
+  search?: string;
 };
 
 type BlogPostsState = {
-    blogPosts: BlogPostsData;
+  blogPosts: BlogPostsData;
 };
 
-export default class BlogPosts extends React.Component<BlogPostsProperties,
-    BlogPostsState> {
-    async componentDidMount() {
-        let blogPosts = await BlogPostsData.QueryRecent();
-        blogPosts = blogPosts.sort();
+export default class BlogPosts extends React.Component<
+  BlogPostsProperties,
+  BlogPostsState
+> {
+  async componentDidMount() {
+    let blogPosts = await BlogPostsData.QueryRecent();
+    blogPosts = blogPosts.sort();
 
-        this.setState({
-            blogPosts: blogPosts
-        });
-    }
+    this.setState({
+      blogPosts: blogPosts
+    });
+  }
 
-    makePost(postData: BlogPostData): JSX.Element {
-        return (
-            <Link key={`postID-${postData.getID}`} to={`/blog/${postData.getID}`} className="column">
-                <article className="message is-dark">
-                    <div className="message-header">
-                        <p>{postData.getCompiledTitle!}</p>
-                    </div>
-                    <div className="message-body">{postData.getDescription}</div>
-                </article>
-            </Link>
+  makePost(postData: BlogPostData): JSX.Element {
+    console.log(postData);
+    return (
+      <Link
+        key={`postID-${postData.getID}`}
+        to={`/blog/${postData.getID}`}
+        className="column"
+      >
+        <article className="message is-dark">
+          <div className="message-header">
+            <p>{postData.getCompiledTitle!}</p>
+          </div>
+          <div className="message-body">{postData.getShortDescription}</div>
+          <div className="is-half has-text-right has-text-black" style={{paddingRight: '8px'}}>
+            Updated on: {postData.getPublishDate}
+          </div>
+        </article>
+      </Link>
+    );
+  }
+
+  makePostRows(blogPostsData: BlogPostsData, size: number): JSX.Element {
+    let blogPosts = blogPostsData.getBlogPosts;
+    let rows: JSX.Element[] = [];
+    let posts: JSX.Element[] = [];
+    let counter = 0;
+    for (let i = 0; i < blogPosts.length; i++) {
+      let blogPost = blogPosts[i];
+
+      counter++;
+      if (counter === size - 1) {
+        rows.push(
+          <section key={`row-${rows.length}`} className="columns">
+            {posts}
+          </section>
         );
+        posts = [];
+        counter = 0;
+      }
+
+      let postJSX = this.makePost(blogPost);
+      posts.push(postJSX);
     }
 
-    makePostRows(blogPostsData: BlogPostsData, size: number): JSX.Element {
-        let blogPosts = blogPostsData.getBlogPosts;
-        let rows: JSX.Element[] = [];
-        let posts: JSX.Element[] = [];
-        let counter = 0;
-        for (let i = 0; i < blogPosts.length; i++) {
-            let blogPost = blogPosts[i];
-
-            counter++;
-            if (counter === size - 1) {
-                rows.push(<section key={`row-${rows.length}`} className="columns">{posts}</section>);
-                posts = [];
-                counter = 0;
-            }
-
-            let postJSX = this.makePost(blogPost);
-            posts.push(postJSX);
-        }
-
-        // Handle rest/leftovers
-        if (posts.length !== 0) {
-            rows.push(<section key={`row-${rows.length}`} className="columns">{posts}</section>);
-        }
-
-        // Handle no rows (=no posts)
-        if (rows.length === 0) {
-            let post = this.makePost(BlogPostData.CreateNothingFoundPost());
-            return <div>{post}</div>;
-        } else {
-            return <div>{rows}</div>;
-        }
+    // Handle rest/leftovers
+    if (posts.length !== 0) {
+      rows.push(
+        <section key={`row-${rows.length}`} className="columns">
+          {posts}
+        </section>
+      );
     }
 
-    render(): JSX.Element {
-        if (this.state == null) {
-            return (
-                <progress className="progress is-large is-info" max="100">
-                    Loading ...
-                </progress>
-            );
-        } else {
-            return <div id="posts">{this.makePostRows(this.state.blogPosts, 3)}</div>;
-        }
+    // Handle no rows (=no posts)
+    if (rows.length === 0) {
+      let post = this.makePost(BlogPostData.CreateNothingFoundPost());
+      return <div>{post}</div>;
+    } else {
+      return <div>{rows}</div>;
     }
+  }
+
+  render(): JSX.Element {
+    if (this.state == null) {
+      return (
+        <progress className="progress is-large is-info" max="100">
+          Loading ...
+        </progress>
+      );
+    } else {
+      return <div id="posts">{this.makePostRows(this.state.blogPosts, 3)}</div>;
+    }
+  }
 }
