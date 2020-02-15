c43f2bf9efa7322bff401f95e76db22d92dcd6fa
Lukas Weber <me@sakul6499.de>
2020-02-15 13:50:12 +0100

    Adjusted docker related files to ARM remotes
    Signed-off-by: Lukas Weber <me@sakul6499.de>

--- make_docker_files.sh
+++ make_docker_files.sh
@@ -5,12 +5,12 @@ shopt -s nocasematch
 
 # Match architecture
 case "$(uname -m)" in
-    "x86_64" ) echo "> Using x86_64 images!"; MONGODB="mongo" ;;
-    "armv7l" ) echo "> Using ARM (armv7l) images!"; MONGODB="mangoraft/mongodb-arm" ;;
+    "x86_64" ) echo "> Using x86_64 images!"; MONGODB="mongo"; JDK="openjdk:11" ;;
+    "armv7l" ) echo "> Using ARM (armv7l) images!"; MONGODB="mangoraft/mongodb-arm"; JDK="arm32v7/openjdk:11" ;;
     *) echo "! Unsupported architecture!"; exit -1;;
 esac
 
 for f in $(/usr/bin/find . -maxdepth 3 -type f \( -name "Dockerfile" -or -name "docker-compose.yml" \)); do
     echo ">>> $f"
-    cat "$f" | sed "s,@MONGODB@,$MONGODB,g" > "$f.out"
+    cat "$f" | sed "s,@MONGODB@,$MONGODB,g" | sed "s,@JDK@,$JDK,g" > "$f.out"
 done
