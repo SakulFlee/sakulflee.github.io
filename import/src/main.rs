4eb366cae56959b517e2d4c5c2860fec87a167da
@Sakul6499 | Lukas Weber <me@sakul6499.de>
2020-06-21 13:32:32 +0200

    Cleanup of importer main

--- import/src/main.rs
+++ import/src/main.rs
@@ -1,37 +1,30 @@
 use data::data::posts::create;
+use data::database::connection::connection_valid;
 use data::models::NewPost;
 use dotenv::dotenv;
 use std::env;
 use std::fs::File;
 use std::io::BufReader;
 use std::io::Read;
+use std::path::PathBuf;
 use walkdir::WalkDir;
 
-fn main() {
-    println!("### Blog importer ###");
+fn clean_database() {
+    data::data::posts::delete_all().expect("Failed to clean database");
+}
+
+fn get_posts_path() -> PathBuf {
     // Initialize dotenv
     dotenv().ok();
-    // Get path for posts
-    let posts_path_env = match env::var("POSTS_PATH") {
-        Ok(var) => {
-            println!("Using path: {}", var);
-            var
-        }
-        Err(e) => {
-            let path = String::from("posts/");
-            println!(
-                "Could not read POSTS_PATH ({})! Defaulting to '{}'",
-                e, path
-            );
-            path
-        }
-    };
 
+    let posts_path_str = env::var("POSTS_PATH").unwrap_or(String::from("posts/"));
     let mut posts_path = env::current_dir().unwrap();
-    posts_path.push(posts_path_env);
-    println!("Posts path: {:?}", posts_path);
+    posts_path.push(posts_path_str);
+    posts_path
+}
 
-    for entry in WalkDir::new(&posts_path)
+fn process_posts(path: PathBuf) {
+    for entry in WalkDir::new(path)
         .min_depth(1)
         .into_iter()
         .filter_entry(|x| x.file_type().is_file())
@@ -66,4 +59,26 @@ fn main() {
         let result = create(post).expect("Failed to create new post");
         println!("New post id: {}", result.id);
     }
+}
+
+fn main() {
+    println!("### Blog post importer ###");
+    println!("Warning: All posts will be removed from the database and posts found locally will be processed and uploaded.");
+
+    let posts_path = get_posts_path();
+
+    if !connection_valid() {
+        println!("Could not connect to database.");
+        println!("Please make sure that an environment variable 'DATABASE_URL' is set properly and that the defined database is accepting connections.");
+        println!(
+            "Currently, the variable is set to: {}",
+            env::var("DATABASE_URL").unwrap_or(String::from("NONE"))
+        );
+        println!("The variable should follow this schema: <protocol>://<username>:<password>@<host>:<port>/<database>");
+        return;
+    }
+
+    clean_database();
+
+    process_posts(posts_path);
 }
