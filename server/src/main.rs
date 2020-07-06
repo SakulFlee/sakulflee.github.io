4533f9e47890e51765c0f8846b32edd4d5e743fe
@Sakul6499 | Lukas Weber <me@sakul6499.de>
2020-07-06 12:56:12 +0200

    Added static-path lookup via dotenv to server

--- server/src/main.rs
+++ server/src/main.rs
@@ -12,12 +12,17 @@ extern crate log;
 pub mod endpoints;
 
 use data::database::migration::run_db_migrations;
+use dotenv::dotenv;
 use rocket::fairing::AdHoc;
 use rocket_contrib::helmet::SpaceHelmet;
 use rocket_contrib::serve::StaticFiles;
 use rocket_contrib::templates::Template;
+use std::env;
 
 fn main() {
+    dotenv().ok();
+    let static_path = env::var("STATIC_PATH").unwrap_or(String::from("static/"));
+
     rocket::ignite()
         .attach(AdHoc::on_attach("Database Migrations", run_db_migrations))
         .register(catchers![endpoints::catch::not_found])
@@ -36,6 +41,6 @@ fn main() {
                 endpoints::page::index_htm,
             ],
         )
-        .mount("/", StaticFiles::from("static/"))
+        .mount("/", StaticFiles::from(static_path))
         .launch();
 }
