2da06f86f9f2f4ae73f5003d4f0231ee53bab7cd
@Sakul6499 | Lukas Weber <me@sakul6499.de>
2020-07-07 23:29:32 +0200

    Removed unnecessary endpoints

--- server/src/endpoints/index.rs
+++ server/src/endpoints/index.rs
@@ -6,18 +6,3 @@ pub fn main_page() -> Template {
     let context = Context::empty();
     Template::render("index", &context)
 }
-
-#[get("/index")]
-pub fn index() -> Template {
-    main_page()
-}
-
-#[get("/index.html")]
-pub fn index_html() -> Template {
-    main_page()
-}
-
-#[get("/index.htm")]
-pub fn index_htm() -> Template {
-    main_page()
-}
