ebd8d68721220586a0a66b0519edcf53434f6cb5
@Sakul6499 | Lukas Weber <me@sakul6499.de>
2020-07-08 17:37:14 +0200

    [WIP] Corrected template in view template

--- server/src/endpoints/blog/view.rs
+++ server/src/endpoints/blog/view.rs
@@ -6,5 +6,5 @@ pub fn blog_view_post(x: String) -> Template {
     println!("{}", x);
     let post = posts::get_by_title(x).expect("Failed to fetch post with given url");
 
-    Template::render("blog/view", &post)
+    Template::render("view", &post)
 }
