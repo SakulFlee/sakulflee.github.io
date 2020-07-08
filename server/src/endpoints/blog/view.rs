d97956b9705956cca44d47a2b11a5f601c0c8278
@Sakul6499 | Lukas Weber <me@sakul6499.de>
2020-07-08 11:27:50 +0200

    Moved view endpoint

---
+++ server/src/endpoints/blog/view.rs
@@ -0,0 +1,10 @@
+use data::data::posts;
+use rocket_contrib::templates::Template;
+
+#[get("/blog/view/<x>")]
+pub fn blog_view_post(x: String) -> Template {
+    println!("{}", x);
+    let post = posts::get_by_title(x).expect("Failed to fetch post with given url");
+
+    Template::render("blog/view", &post)
+}
