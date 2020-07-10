3d266eb3b97da9ab1b9f8cbf91c257832f6836d1
@Sakul6499 | Lukas Weber <me@sakul6499.de>
2020-07-10 23:01:24 +0200

    [WIP] Added about me, timeline and skills to index

--- server/src/endpoints/about_me.rs
+++ server/src/endpoints/about_me.rs
@@ -1,5 +1,8 @@
+use crate::Template;
+use std::collections::HashMap;
+
 #[get("/about_me")]
-pub fn about_me() -> &'static str {
-    // Template::render("view", &context)
-    "<h1>WIP</h1>"
+pub fn about_me() -> Template {
+    let context: HashMap<String, String> = HashMap::new();
+    Template::render("about_me", context)
 }
