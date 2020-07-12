49cd401664eefc59990eee12146fcab36fbee1d1
@Sakul6499 | Lukas Weber <me@sakul6499.de>
2020-07-12 13:49:15 +0200

    Added birthdate / age calculation to index endpoint context

--- server/src/endpoints/index.rs
+++ server/src/endpoints/index.rs
@@ -1,8 +1,18 @@
-use data::models::context::Context;
+use chrono::offset::TimeZone;
+use chrono::Date;
+use chrono::Utc;
 use rocket_contrib::templates::Template;
+use std::collections::HashMap;
 
 #[get("/")]
 pub fn main_page() -> Template {
-    let context = Context::empty();
-    Template::render("index", &context)
+    let utc: Date<Utc> = Utc::today();
+    let birth_date = Utc.ymd(1999, 4, 6);
+
+    let duration = utc - birth_date;
+    let age = duration.num_days() / 365;
+
+    let mut map: HashMap<&str, i64> = HashMap::new();
+    map.insert("age", age);
+    Template::render("index", &map)
 }
