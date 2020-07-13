5863dd14633b32cf96a309162e9dd3ea2a530ea7
@Sakul6499 | Lukas Weber <me@sakul6499.de>
2020-07-13 13:58:33 +0200

    Moved legal template

--- server/src/endpoints/legal.rs
+++ server/src/endpoints/legal.rs
@@ -4,5 +4,5 @@ use std::collections::HashMap;
 #[get("/legal")]
 pub fn legal() -> Template {
     let context = HashMap::<String, String>::new();
-    Template::render("components/legal", &context)
+    Template::render("legal", &context)
 }
