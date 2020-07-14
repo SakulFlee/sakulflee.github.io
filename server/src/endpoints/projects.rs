276613ec0bae7328badce5b1f66c55fafdd827f2
@Sakul6499 | Lukas Weber <me@sakul6499.de>
2020-07-14 12:53:34 +0200

    Removed project endpoint

--- server/src/endpoints/projects.rs
+++
@@ -1,8 +0,0 @@
-use rocket_contrib::templates::Template;
-use std::collections::HashMap;
-
-#[get("/projects")]
-pub fn projects() -> Template {
-    let context = HashMap::<String, String>::new();
-    Template::render("projects", &context)
-}
